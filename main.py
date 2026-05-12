c53e7f4e40ac31225e32186f5ee887ee374dec96
Don Moreno <d.moreno@esis-italia.com>
2026-05-12 17:09:46 +0200

    feat: buttons

--- main.py
+++ main.py
@@ -1,77 +1,26 @@
-import sys # sys stands for 'system'
-            # It is a built-in Python module that gives your program access to:
-
-            # the Python interpreter itself
-            # command-line arguments
-            # program exit behavior
-            # standard input/output
-            # recursion limits
-            # Python paths
-
-            # Think of it like:
-            # os talks to the operating system
-            # sys talks to the Python runtime itself
-from PyQt5.QtWidgets import ( QApplication, QMainWindow, QLabel,
-                            QWidget, QVBoxLayout, QHBoxLayout, QGridLayout)
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("My cool first GUI")
         self.setGeometry(700, 250, 500, 500)
+        self.button = QPushButton("Click me!", self)
         self.initUI()
         
     def initUI(self):
-        central_widget = QWidget()
-        self.setCentralWidget(central_widget)
-        
-        label1 = QLabel("#1", self)
-        label2 = QLabel("#2", self)
-        label3 = QLabel("#3", self)
-        label4 = QLabel("#4", self)
-        label5 = QLabel("#5", self)
-        
-        label1.setStyleSheet("background-color: red;")
-        label2.setStyleSheet("background-color: yellow;")
-        label3.setStyleSheet("background-color: green;")
-        label4.setStyleSheet("background-color: blue;")
-        label5.setStyleSheet("background-color: purple;")
-        
-        # vbox = QVBoxLayout()
-        
-        # vbox.addWidget(label1)
-        # vbox.addWidget(label2)
-        # vbox.addWidget(label3)
-        # vbox.addWidget(label4)
-        # vbox.addWidget(label5)
-        
-        # central_widget.setLayout(vbox)
-        
-        # hbox = QHBoxLayout()
-        
-        # hbox.addWidget(label1)
-        # hbox.addWidget(label2)
-        # hbox.addWidget(label3)
-        # hbox.addWidget(label4)
-        # hbox.addWidget(label5)
-        
-        # central_widget.setLayout(hbox)
-        
-        grid = QGridLayout()
-        
-        grid.addWidget(label1, 0, 0)
-        grid.addWidget(label2, 0 ,1)
-        grid.addWidget(label3, 1, 0)
-        grid.addWidget(label4, 1, 1)
-        grid.addWidget(label5, 1, 2)
-        
-        central_widget.setLayout(grid)
-    
-def main():
+        self.button.setGeometry(150, 200, 200, 100)
+        self.button.setStyleSheet("font-size: 30px;")
+        self.button.clicked.connect(self.on_click)
+
+    def on_click(self):
+        print("Button clicked!")
+        self.button.setText("Clicked!")
+        self.button.setDisabled(True)
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
-
-if __name__ == "__main__":
-    main()
