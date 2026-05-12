2a081482a54ad839a2f764dac29f996a7232ee8e
Don Moreno <d.moreno@esis-italia.com>
2026-05-12 16:59:19 +0200

    feat: layout manager

--- main.py
+++ main.py
@@ -11,40 +11,62 @@ import sys # sys stands for 'system'
             # Think of it like:
             # os talks to the operating system
             # sys talks to the Python runtime itself
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import ( QApplication, QMainWindow, QLabel,
+                            QWidget, QVBoxLayout, QHBoxLayout, QGridLayout)
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("My cool first GUI")
         self.setGeometry(700, 250, 500, 500)
+        self.initUI()
         
-        label = QLabel(self)
-        label.setGeometry(0, 0, 250, 250)
+    def initUI(self):
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
         
-        pixmap = QPixmap("my_picture.jpg")
-        label.setPixmap(pixmap)
-
-        label.setScaledContents(True)
-        # BOTTOM RIGHT
-        # label.setGeometry(self.width() - label.width(),
-        #                   self.height() - label.height(),
-        #                   label.width(),
-        #                   label.height())
+        label1 = QLabel("#1", self)
+        label2 = QLabel("#2", self)
+        label3 = QLabel("#3", self)
+        label4 = QLabel("#4", self)
+        label5 = QLabel("#5", self)
         
-        # BOTTOM LEFT
-        # label.setGeometry(0,
-        #                   self.height() - label.height(),
-        #                   label.width(),
-        #                   label.height())
+        label1.setStyleSheet("background-color: red;")
+        label2.setStyleSheet("background-color: yellow;")
+        label3.setStyleSheet("background-color: green;")
+        label4.setStyleSheet("background-color: blue;")
+        label5.setStyleSheet("background-color: purple;")
         
-        # CENTER
-        label.setGeometry((self.width() - label.width()) // 2,
-                          (self.height() - label.height()) // 2,
-                          label.width(),
-                          label.height())
+        # vbox = QVBoxLayout()
         
+        # vbox.addWidget(label1)
+        # vbox.addWidget(label2)
+        # vbox.addWidget(label3)
+        # vbox.addWidget(label4)
+        # vbox.addWidget(label5)
+        
+        # central_widget.setLayout(vbox)
+        
+        # hbox = QHBoxLayout()
+        
+        # hbox.addWidget(label1)
+        # hbox.addWidget(label2)
+        # hbox.addWidget(label3)
+        # hbox.addWidget(label4)
+        # hbox.addWidget(label5)
+        
+        # central_widget.setLayout(hbox)
+        
+        grid = QGridLayout()
+        
+        grid.addWidget(label1, 0, 0)
+        grid.addWidget(label2, 0 ,1)
+        grid.addWidget(label3, 1, 0)
+        grid.addWidget(label4, 1, 1)
+        grid.addWidget(label5, 1, 2)
+        
+        central_widget.setLayout(grid)
+    
 def main():
     app = QApplication(sys.argv)
     window = MainWindow()
