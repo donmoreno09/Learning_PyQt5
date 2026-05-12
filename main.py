f54b18a0c55d80297dfe7d8ea730c681a136cf58
Don Moreno <d.moreno@esis-italia.com>
2026-05-12 16:30:01 +0200

    feat: learning label and some Qt position

--- main.py
+++ main.py
@@ -11,15 +11,37 @@ import sys # sys stands for 'system'
             # Think of it like:
             # os talks to the operating system
             # sys talks to the Python runtime itself
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("My cool first GUI")
         self.setGeometry(700, 250, 500, 500)
-        self.setWindowIcon(QIcon("/home/esis/Python_Learning/pyqt5/my_picture.jpg"))
+        
+        label = QLabel("Hello", self)
+        label.setFont(QFont("Arial", 40))
+        label.setGeometry(0, 0, 500, 100)
+        label.setStyleSheet("color: #292929;"
+                            "background-color: #6fdcf7"
+                            "font-weight: bold;"
+                            "font-style: italic;"
+                            "text-decoration: underline;")
+        
+        # label.setAlignment(Qt.AlignTop) # VERTICALLY TOP
+        # label.setAlignment(Qt.AlignBottom) # VERTICALLY BOTTOM
+        # label.setAlignment(Qt.AlignVCenter) # VERTICALLY CENTER
+        # label.setAlignment(Qt.AlignRight) # HORIZONTALLY RIGHT
+        # label.setAlignment(Qt.AlignHCenter) # HORIZONTALLY CENTER
+        # label.setAlignment(Qt.AlignLeft) # HORIZONTALLY LEFT
+        
+        # Combination of positions using '|'
+        # label.setAlignment(Qt.AlignHCenter | Qt.AlignTop) # CENTER & TOP
+        # label.setAlignment(Qt.AlignHCenter | Qt.AlignBottom) # CENTER & BOTTOM
+        # label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter) # CENTER & CENTER
+        label.setAlignment(Qt.AlignCenter) # CENTER & CENTER
 
 def main():
     app = QApplication(sys.argv)
